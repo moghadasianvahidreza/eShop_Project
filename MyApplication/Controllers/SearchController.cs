c6b741ed06ab3e19395c3a1f465a52c9464c288d
vahidreza moghadasian <moghadasian.vahidreza@hotmail.com>
2020-05-12 00:29:07 +0430

    Make Some Changes => Add Search bar Product : 99/02/23 - 12:29

---
+++ MyApplication/Controllers/SearchController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyApplication.Controllers
+{
+	public class SearchController : Infrastructure.BaseController
+	{
+		public SearchController() : base()
+		{
+
+		}
+
+		// GET: Search
+		public ActionResult Index(string text)
+		{
+			List<Models.Product> list = new List<Models.Product>();
+
+			list.AddRange(DatabaseContext.ProductTags
+				.Where(current => current.Tag == text)
+				.Select(current => current.Product)
+				.ToList()
+				);
+
+			list.AddRange(DatabaseContext.Products.Where(
+				current => current.ProductTitle.Contains(text) || 
+				current.ShortDescription.Contains(text) || 
+				current.Text.Contains(text))
+				.ToList()
+				);
+
+			ViewBag.search = text;
+
+			return View(list.Distinct());
+		}
+	}
+}

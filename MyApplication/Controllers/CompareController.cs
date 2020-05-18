72b942bf4e1bc73e366157fad543695ae5c28d24
vahid reza <moghadasian.vahidreza@gmail.com>
2020-05-18 16:49:36 +0430

    Make Some Changes => Add Compare Products : 99/02/29 - 04:49

---
+++ MyApplication/Controllers/CompareController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyApplication.Controllers
+{
+	public class CompareController : Infrastructure.BaseController
+	{
+		// GET: Compare
+		public ActionResult Index()
+		{
+			List<ViewModels.CompareItemViewModel> list = new List<ViewModels.CompareItemViewModel>();
+
+			if (Session["Compare"] != null)
+			{
+				list = Session["Compare"] as List<ViewModels.CompareItemViewModel>;
+			}
+
+			List<Models.Feature> features = new List<Models.Feature>();
+			List<Models.ProductFeature> productFeature = new List<Models.ProductFeature>();
+
+			foreach (var item in list)
+			{
+				features.AddRange(DatabaseContext.ProductFeatures
+							.Where(current => current.ProductId == item.ProductId)
+						.Select(p => p.Feature)
+						.ToList())
+						;
+
+				productFeature.AddRange(DatabaseContext.ProductFeatures
+								.Where(current => current.ProductId == item.ProductId)
+								.ToList())
+								;
+			}
+
+			ViewBag.features = features.Distinct().ToList();
+			ViewBag.productFeatures = productFeature;
+
+			return View(list);
+		}
+
+		// ******************************
+		// ******************************
+
+		public ActionResult AddToCompare(int id)
+		{
+			List<ViewModels.CompareItemViewModel> list = new List<ViewModels.CompareItemViewModel>();
+
+			if (Session["Compare"] != null)
+			{
+				list = Session["Compare"] as List<ViewModels.CompareItemViewModel>;
+			}
+
+			if (!list.Any(current => current.ProductId == id))
+			{
+				var product = DatabaseContext.Products.Where(current => current.ProductId == id).Select(p => new
+				{
+					p.ProductTitle,
+					p.ImageName,
+				}).FirstOrDefault();
+
+				list.Add(new ViewModels.CompareItemViewModel()
+				{
+					ProductId = id,
+					Title = product.ProductTitle,
+					ImageName = product.ImageName,
+				});
+			}
+
+			Session["Compare"] = list;
+
+			return PartialView("ListCompare", list);
+		}
+
+		// ******************************
+		// ******************************
+
+		public ActionResult ListCompare()
+		{
+			List<ViewModels.CompareItemViewModel> list = new List<ViewModels.CompareItemViewModel>();
+
+			if (Session["Compare"] != null)
+			{
+				list = Session["Compare"] as List<ViewModels.CompareItemViewModel>;
+			}
+
+			return PartialView(list);
+		}
+
+		// ******************************
+		// ******************************
+
+		public ActionResult DeleteListCompare(int id)
+		{
+			List<ViewModels.CompareItemViewModel> list = new List<ViewModels.CompareItemViewModel>();
+
+			if (Session["Compare"] != null)
+			{
+				list = Session["Compare"] as List<ViewModels.CompareItemViewModel>;
+				int index = list.FindIndex(current => current.ProductId == id);
+				list.RemoveAt(index);
+				Session["Compare"] = list;
+			}
+
+			return PartialView("ListCompare", list);
+		}
+	}
+}
---
+++ ViewModels/CompareItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+	public class CompareItemViewModel : object
+	{
+		public CompareItemViewModel() : base()
+		{
+
+		}
+
+		public int ProductId { get; set; }
+
+		public string Title { get; set; }
+
+		public string ImageName { get; set; }
+	}
+}
